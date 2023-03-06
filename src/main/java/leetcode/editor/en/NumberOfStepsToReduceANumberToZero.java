231498f17203051dd393f5cf6237c869137a9a20
gordon.lee <gordon.lee@okg.com>
2023-03-06 23:25:29 +0800

    1342 Number of Steps to Reduce a Number to Zero

---
+++ src/main/java/leetcode/editor/en/NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,27 @@
+package leetcode.editor.en;
+
+public class NumberOfStepsToReduceANumberToZero {
+    public static void main(String[] args) {
+        new NumberOfStepsToReduceANumberToZero().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        int numOfSteps = 0;
+
+        public int numberOfSteps(int num) {
+            if (num == 0) {
+                return numOfSteps;
+            }
+            numOfSteps++;
+            if (num % 2 == 0) {
+                numberOfSteps(num / 2);
+            } else if (num % 2 == 1) {
+                numberOfSteps(num - 1);
+            }
+            return numOfSteps;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
