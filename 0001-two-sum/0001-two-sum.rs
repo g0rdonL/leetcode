dc7ebee50ae1ca90ae97df921912f3b6c904f608
Gordon Lee <hkgordonlee@gmail.com>
2025-05-23 15:55:37 +0800

    Time: 0 ms (100%), Space: 2.6 MB (5.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.rs
+++ 0001-two-sum/0001-two-sum.rs
@@ -2,16 +2,14 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map = HashMap::new();
-
+        let mut num_map = HashMap::new(); 
         for (i, &num) in nums.iter().enumerate() {
-            let complement = target - num;
-            if let Some(&complement_index) = map.get(&complement) {
-                return vec![complement_index as i32, i as i32];
+            let complement = target - num; 
+            if let Some(&index) = num_map.get(&complement) {
+                return vec![index as i32, i as i32]; 
             }
-            map.insert(num, i);
+            num_map.insert(num, i); 
         }
-
         vec![] 
     }
 }
